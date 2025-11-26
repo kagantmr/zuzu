e220b16a0b998f312680394ff7be4c907cf7dff3
Kağan T. <kagantemur123@gmail.com>
2025-11-26 13:30:13 +0300

    Worked on boot process and memory management; update linker script, stack handling, and kernel layout

--- src/arch/arm/boot/_start.s
+++ src/arch/arm/boot/_start.s
@@ -1,12 +1,19 @@
 .global _start
 _start:
-    ldr sp, =boot_stack_top @ Initialize stack pointer
+    ldr sp, =__stack_base__    @ Initialize stack pointer
+    bl clear_bss              @ Clear BSS section  
     bl kernel_early           @ Jump to C code
-1:  b 1b               @ Loop forever
+1:  b 1b                      @ Loop forever
 .size _start, . - _start
 
-.bss
-    .align 3          @ 8-byte alignment for ARM EABI
-boot_stack:
-    .space 1024       @ 1 KB
-boot_stack_top:
+clear_bss:
+    ldr r0, =_bss_start
+    ldr r1, =_bss_end
+    mov r2, #0
+1:
+    cmp r0, r1
+    bge 2f
+    str r2, [r0], #4
+    b 1b
+2:
+    bx lr
