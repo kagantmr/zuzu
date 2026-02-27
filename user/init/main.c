4d12993710fbcba1fcd2984fe5ab925308692355
kagantmr <mkagantemur@gmail.com>
2026-02-27 19:50:42 +0300

    test: bss clearing

--- user/init/main.c
+++ user/init/main.c
@@ -1,5 +1,7 @@
 #include "zuzu.h"
 
+int big_array[256];  // forces a BSS segment
+
 int main() {
     const char hello_string[] = "Hello from zuzu ELF!\n";
     _log(hello_string, sizeof(hello_string) - 1);
