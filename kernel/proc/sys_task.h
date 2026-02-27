317f3f9f5a5d92e3997ec8fa937340c64fbfea2b
kagantmr <mkagantemur@gmail.com>
2026-02-27 19:55:44 +0300

    feat: bss zeroed upon entry

--- kernel/proc/sys_task.h
+++ kernel/proc/sys_task.h
@@ -5,9 +5,10 @@
 
 void sys_task_quit(exception_frame_t *frame);
 void sys_task_yield(exception_frame_t *frame);
-void sys_log(exception_frame_t *frame);
 void sys_task_sleep(exception_frame_t *frame);
 void sys_get_pid(exception_frame_t *frame);
+void sys_task_wait(exception_frame_t *frame);
+void sys_task_spawn(exception_frame_t *frame);
 
 
 #endif // SYS_TASK_H
