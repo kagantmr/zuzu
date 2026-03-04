24fb03a3f167b2173d15afb463e6edb54b001f70
kagantmr <mkagantemur@gmail.com>
2026-03-04 18:36:56 +0300

    fix: zuart bugs

--- user/hello/main.c
+++ user/hello/main.c
@@ -2,6 +2,14 @@
 #include "nt_protocol.h"
 #include "zuart_protocol.h"
 
+static void zuart_write_str(int handle, const char *s)
+{
+    for (const char *p = s; *p; p++)
+    {
+        _call(handle, ZUART_CMD_WRITE, (uint32_t)(unsigned char)*p, 0);
+    }
+}
+
 int main(void)
 {
     _sleep(10);
@@ -14,7 +22,20 @@ int main(void)
 
     int uart_handle = reply.r2; // slot in OUR table
 
-    // send one char at a time... for now
+    // blocking-read test: this call should block until a byte is received by zuart
+    zuart_write_str(uart_handle, "hello: type one key for blocking read test...\n");
+    zuzu_ipcmsg_t read_reply = _call(uart_handle, ZUART_CMD_READ, 0, 0);
+    if ((int32_t)read_reply.r2 == 0)
+    {
+        _call(uart_handle, ZUART_CMD_WRITE, read_reply.r1, 0);
+        zuart_write_str(uart_handle, " <- hello got your key\n");
+    }
+    else
+    {
+        zuart_write_str(uart_handle, "hello: read failed\n");
+    }
+
+    // periodic writer
     while (1)
     {
         const char *msg = "hello: i'm still awake\n";
