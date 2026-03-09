93472066f64d1f6778838e33cb836920afb189e9
kagantmr <mkagantemur@gmail.com>
2026-03-09 14:45:15 +0300

    refactor(version): Major, minor, patch defines, v removed from version string

--- core/version.h
+++ core/version.h
@@ -1,6 +1,9 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define ZUZU_VERSION "v0.1.1-alpha"
+#define ZUZU_VERSION_MAJOR 0
+#define ZUZU_VERSION_MINOR 1
+#define ZUZU_VERSION_PATCH 1
+#define ZUZU_VERSION "0.1.1-alpha"
 
 #endif
