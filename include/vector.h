cb8b93287e3554fb9feeb5fcad337dd651b22961
kagantmr <mkagantemur@gmail.com>
2026-04-02 12:30:06 +0300

    fix(vector): vector library is agnostic of the includer

--- include/vector.h
+++ include/vector.h
@@ -3,8 +3,19 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "kernel/mm/alloc.h"
 #include <mem.h>
+
+#ifdef __KERNEL__
+#include "kernel/mm/alloc.h"
+#define VEC_ALLOC(sz) kmalloc(sz)
+#define VEC_FREE(ptr) kfree(ptr)
+#else
+#include <zmalloc.h>
+#define VEC_ALLOC(sz) zmalloc(sz)
+#define VEC_FREE(ptr) zfree(ptr)
+#endif
+
+#define VEC_ALLOC_T(type, count) ((type *)VEC_ALLOC((count) * sizeof(type)))
 
 #define VEC_INITIAL_CAP 16
 
@@ -19,7 +30,7 @@
                                                                           \
     static inline bool name##_vec_init(name##_vec_t *v)                   \
     {                                                                     \
-        v->data = kmalloc(VEC_INITIAL_CAP * sizeof(type));                \
+        v->data = VEC_ALLOC_T(type, VEC_INITIAL_CAP);                     \
         if (!v->data)                                                     \
             return false;                                                 \
         v->len = 0;                                                       \
@@ -38,12 +49,12 @@
     static inline int name##_vec_grow(name##_vec_t *v)                    \
     {                                                                     \
         uint32_t new_cap = v->cap * 2;                                    \
-        type *new_data = kmalloc(new_cap * sizeof(type));                 \
+        type *new_data = VEC_ALLOC_T(type, new_cap);                      \
         if (!new_data)                                                    \
             return -1;                                                    \
         memset(new_data, 0, new_cap * sizeof(type));                      \
         memmove(new_data, v->data, v->cap * sizeof(type));                \
-        kfree(v->data);                                                   \
+        VEC_FREE(v->data);                                                \
         v->data = new_data;                                               \
         v->cap = new_cap;                                                 \
         return 0;                                                         \
@@ -51,7 +62,7 @@
                                                                           \
     static inline void name##_vec_destroy(name##_vec_t *v)                \
     {                                                                     \
-        kfree(v->data);                                                   \
+        VEC_FREE(v->data);                                                \
         v->data = NULL;                                                   \
         v->len = 0;                                                       \
         v->cap = 0;                                                       \
