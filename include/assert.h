7274091c4d387f0dc0ea567be89499131cdd2794
kagantmr <mkagantemur@gmail.com>
2026-05-02 21:22:23 +0300

    refactor(kassert): rename to assert

--- include/assert.h
+++ include/assert.h
@@ -2,15 +2,15 @@
 #define ZCRT_ASSERT_H
 
 #ifdef NDEBUG
-    #define kassert(cond) ((void)0)
+    #define assert(cond) ((void)0)
 #else
     #ifdef __KERNEL__
         #include <core/panic.h>
-        #define kassert(cond) if(!(cond)) panic("Assertion failed: " #cond)
+        #define assert(cond) if(!(cond)) panic("Assertion failed: " #cond)
     #else
         #include <zuzu.h>
         // User space assert is just quit for now
-        #define kassert(cond) if(!(cond)) { _quit(-1); } 
+        #define assert(cond) if(!(cond)) { _quit(-1); } 
     #endif
 #endif
 
