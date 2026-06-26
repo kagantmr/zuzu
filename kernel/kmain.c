57a4c04c07b98c4ddb01b7dad0a0496b01dd224f
kagantmr <mkagantemur@gmail.com>
2026-06-26 18:38:50 +0300

    refactor(arch): move shared early boot code to arch folder

--- kernel/kmain.c
+++ kernel/kmain.c
@@ -2,7 +2,7 @@
 #include "arch/arm/include/irq.h"
 
 #include "arch/arm/mmu/mmu.h"
-#include "arch/arm/vexpress-a15/board.h"
+#include "arch/arm/include/board.h"
 
 #include "drivers/uart/uart.h"
 #include "drivers/uart/pl011.h"
