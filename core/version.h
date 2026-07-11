26d04cb2a7ebab5f132bd9ce7fbd960849e1c71c
kagantmr <mkagantemur@gmail.com>
2026-07-11 15:59:12 +0300

    feat(version): OS and kernel version seperated

--- core/version.h
+++ core/version.h
@@ -1,12 +1,12 @@
 // version.h - Kernel version information and macros
 
-#ifndef VERSION_H
-#define VERSION_H
+#ifndef ZUZU_VERSION_H
+#define ZUZU_VERSION_H
 
 #define ZUZU_VERSION_MAJOR 0
 #define ZUZU_VERSION_MINOR 1
 #define ZUZU_VERSION_PATCH 5
-#define ZUZU_CODENAME "loaf"
-#define ZUZU_VERSION "v0.1.5"
+#define ZUZU_CODENAME ""
+#define ZUZU_VERSION "0.1.5"
 
 #endif
