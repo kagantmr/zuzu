317a77a355e8e6913ba46c0a0657dd68dcdc884b
kagantmr <mkagantemur@gmail.com>
2026-07-11 15:59:12 +0300

    feat(version): OS and kernel version seperated

---
+++ include/zuzu/version.h
@@ -0,0 +1,12 @@
+// version.h - zuzuOS version information
+
+#ifndef ZUZUOS_VERSION_H
+#define ZUZUOS_VERSION_H
+
+#define ZUZUOS_VERSION_MAJOR 0
+#define ZUZUOS_VERSION_MINOR 1
+#define ZUZUOS_VERSION_PATCH 5
+#define ZUZUOS_VERSION "v0.1.5-alpha"
+#define ZUZUOS_MIN_KERNEL_MAJOR 1
+
+#endif
