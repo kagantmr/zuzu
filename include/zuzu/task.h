08c9762ff7ede82b8569f43380e454314ff10b51
kagantmr <mkagantemur@gmail.com>
2026-08-22 17:46:20 +0300

    refactor(zcrt): since transitive include from types is gone manually include err.h

--- include/zuzu/task.h
+++ include/zuzu/task.h
@@ -2,7 +2,8 @@
 #define ZUZU_TASK_H
 
 #include "zuzu/syscall_nums.h"
-#include "zuzu/types.h"
+#include <zuzu/types.h>
+#include <zuzu/err.h>
 #include <arch/syscall.h>
 #include <zuzu/spawn_args.h>
 #include <stdint.h>
