999c0624f2fb190ac66d4f9ee4503975ffb5fb4b
kagantmr <mkagantemur@gmail.com>
2026-08-22 17:51:02 +0300

    feat(kevent): Processes can now subscribe to kernel events (kevent) in notifications and take action.
    The first kernel event is a memory pressure flag, which signals inbetween 70-80% usage.

---
+++ include/zuzu/event.h
@@ -0,0 +1,14 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <zuzu/types.h>
+#include "syscall_nums.h"
+#include <arch/syscall.h>
+
+#define KEVENT_MEMMGMT_BIT (1u << 0)
+
+static inline void *ZuzuKEventBind(KEventType eventtype, Handle ntfn, void *args) {
+    return (void *)(VirtAddr)Syscall(SYS_KEVENT_BIND, eventtype, ntfn, (VirtAddr)args, 0);
+}
+
+#endif /* EVENT_H */
