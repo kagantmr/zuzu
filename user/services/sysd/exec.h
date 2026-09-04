7c1cbb27ffecfd62de546998f7ae7e225e5d1102
kagantmr <mkagantemur@gmail.com>
2026-09-04 21:22:34 +0300

    feat(sysd): add ZXF parser to sysd

--- user/services/sysd/exec.h
+++ user/services/sysd/exec.h
@@ -5,7 +5,8 @@
 #include <stdint.h>
 #include <zuzu/protocols/exec.h>
 
-int exec_inject(uint32_t taskHandle, const void *elf_data, size_t elf_size,
+/* Accepts either an ELF or a ZXF image, dispatching on magic. */
+int exec_inject(uint32_t taskHandle, const void *data, size_t size,
                 const char *argbuf, size_t argbuf_len, uint32_t argc,
                 ExecReply *out);
 
