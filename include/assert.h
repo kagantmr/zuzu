30a48d0f0c6d3466698bb00e49a4cac898617a8e
kagantmr <mkagantemur@gmail.com>
2026-09-11 10:44:31 +0300

    fix(assert): wrap in do-while to prevent issues with unwrapping
    Refs #14

--- include/assert.h
+++ include/assert.h
@@ -10,11 +10,13 @@ extern "C" {
 #else
     #ifdef __ZUZU__
         #include <core/panic.h>
-        #define assert(cond) if(!(cond)) panic("Assertion failed: %s (%s:%d)", #cond, __FILE__, __LINE__)
+        #define assert(cond) \
+            do { if (!(cond)) panic("Assertion failed: %s (%s:%d)", #cond, __FILE__, __LINE__); } while (0)
     #else
         #include <zuzu/zuzu.h>
         // User space assert is just quit for now
-        #define assert(cond) if(!(cond)) { ZuzuPQuit(-1); }
+        #define assert(cond) \
+            do { if (!(cond)) { ZuzuPQuit(-1); } } while (0)
     #endif
 #endif
 
