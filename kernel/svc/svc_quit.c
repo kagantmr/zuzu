00e7423039e32de0014cff67077ef2ee4e4d0d29
kagantmr <mkagantemur@gmail.com>
2026-09-22 21:30:56 +0300

    fix(svc): route quit through TaskTerminate

--- kernel/svc/svc_quit.c
+++ kernel/svc/svc_quit.c
@@ -1,28 +1,15 @@
 #include "svc.h"
-#include "kernel/proc/process.h"
 #include "kernel/sched/sched.h"
+#include "kernel/task/task.h"
 
 #define LOG_FMT(fmt) "(SvcQuit) " fmt
 #include <zuzu/log.h>
 
 void SvcQuit(CpuState *frame)
 {
-    int exit_status = (int)(*arch_reg(frame, 0));
+    int32_t exit_status = (int32_t)(*arch_reg(frame, 0));
 
-    SpaceObject *owner = current_thread->owner_process;
-    current_thread->exit_status = exit_status;
-    WakeJoinTask(current_thread, exit_status);
-
-    if (list_one_elem(&owner->threads)) {
-        // last thread, kill the process
-        ProcessKill(owner, exit_status);
-    } else {
-        KillTask(current_thread);
-        // remove from process thread list NOW so process_destroy won't see it
-        if (current_thread->process_node.prev && current_thread->process_node.next)
-            list_remove(&current_thread->process_node);
-        SchedQueueDestroyThread(current_thread);
-    }
+    TaskTerminate(current_thread, exit_status);
 
     Schedule();
 }
