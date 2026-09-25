c3682fc4b62bde986aa67c8a05de3656c4fd3a97
kagantmr <mkagantemur@gmail.com>
2026-09-25 11:11:08 +0300

    fix(irq_relay): change order of irq unbind and drop ref

--- kernel/irq/irq_relay.c
+++ kernel/irq/irq_relay.c
@@ -62,8 +62,8 @@ Err IrqBindToEvent(SpaceObject *owner, Irq irq_num, EventObject *ev)
     if (irq_owners[irq_num].bound_ev)
     {
         EventObject *old = irq_owners[irq_num].bound_ev;
-        EventDropReference(old);
         old->irq_bind_count--;
+        EventDropReference(old);
     }
 
     irq_owners[irq_num].bound_ev = ev;
@@ -98,8 +98,8 @@ void IrqReleaseAll(SpaceObject *owner)
     {
         if (irq_owners[irq_num].owner == owner)
         {
-            EventDropReference(irq_owners[irq_num].bound_ev);
             irq_owners[irq_num].bound_ev->irq_bind_count--;
+            EventDropReference(irq_owners[irq_num].bound_ev);
             irq_owners[irq_num] = (IrqOwner){.bound_ev = NULL, .owner = NULL, .pending = false};
             ArchIrqMaskLine(irq_num);
         }
